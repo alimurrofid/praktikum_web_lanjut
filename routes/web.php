3fe4f1aab8faeeadc5c80b3448ea1e90557ae84c
alimurrofid <alimurrofid77@gmail.com>
2023-05-06 10:10:05 +0700

    routing view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home',function(){return view('home');})->name('home');
+Route::get('/about-us',function(){return view('about-us');})->name('about-us');
+Route::get('/contact-us',function(){return view('contact-us');})->name('contact-us');
+Route::get('/news',function(){return view('news');})->name('news');
+Route::get('/product',function(){return view('product');})->name('product');
+Route::get('/program',function(){return view('program');})->name('program');
